7884aff78e1176cb2351fdd220894180d9d006d9
Magno Sousa <magno.bds@gmail.com>
2019-04-15 21:53:41 -0300

    Adjusts in RouterCollection to generalize your use

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,8 +4,43 @@ namespace MagnoKsm\Router;
 
 class Router
 {
-    public function __construct()
+    private $collection;
+    private $method;
+    private $path;
+
+    public function __construct(string $method, string $path)
     {
-        echo 'its work';
+        $this->collection = new RouterCollection();
+        $this->method = $method;
+        $this->path = $path;
+    }
+
+    public function get($path, $fn)
+    {
+        $this->request('GET', $path, $fn);
+    }
+
+    public function post($path, $fn)
+    {
+        $this->request('POST', $path, $fn);
+    }
+
+    public function request($method, $path, $fn)
+    {
+        $this->collection->add($method, $path, $fn);
+    }
+
+    public function run()
+    {
+        $data = $this->collection->filter($this->method);
+
+        foreach ($data as $key => $value) {
+            $result = $this->checkUrl($key, $this->path);
+        }
+    }
+
+    public function checkUrl()
+    {
+
     }
 }
